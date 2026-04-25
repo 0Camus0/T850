28a3e0ea47334468125d0a7efaeb5f4715d9ad4d
Daniel <camus_mm@hotmail.com>
2026-04-25 14:44:21 -0700

    Add runtime config JSON support

---
+++ T850/Framework/include/core/t8config.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <string>
+
+namespace t800 {
+
+class t8config {
+public:
+  struct BooleanFlags {
+    bool dumpEnabled : 1 = false;
+    bool dumpByFrame : 1 = false;
+    bool debugFrames : 1 = false;
+    bool debugDumpRequested : 1 = false;
+    bool keepRunning : 1 = false;
+    bool fullscreen : 1 = false;
+    bool guiOnStart : 1 = false;
+    bool guiScreenshot : 1 = false;
+    bool guiEdit : 1 = false;
+    bool guiSnap : 1 = false;
+    bool guiControlEdit : 1 = false;
+    bool d3d12Debug : 1 = false;
+    bool testGui : 1 = false;
+    bool createAtlas : 1 = false;
+    bool profile : 1 = false;
+    bool dumpMatrices : 1 = false;
+  } flags;
+
+  std::string api = "d3d11";
+  int width = 1280;
+  int height = 720;
+  std::string title = "T800 Project";
+
+  int dumpFrame = -1;
+  float dumpSeconds = -1.0f;
+  std::string replaySnapshotPath;
+  int startScene = 0;
+
+  std::string guiScreenshotPath = "gui_screenshot.ppm";
+  std::string guiControlTarget = "slider_knob";
+
+  int logLevel = 3;
+  std::string logFile;
+
+  int atlasMaxSprite = 256;
+  int profileFrames = 300;
+  int dumpMatricesFrames = 0;
+  std::string modelPath = "Models/DamagedHelmet.glb";
+};
+
+extern t8config g_t8config;
+
+} // namespace t800

0afa82ab4824dee0e1f22c99f07bda1bf58b797b
Daniel <camus_mm@hotmail.com>
2026-05-05 22:22:49 -0700

    Guard thread pool blocking calls

--- T850/Framework/src/utils/ThreadPool.cpp
+++ T850/Framework/src/utils/ThreadPool.cpp
@@ -1,12 +1,27 @@
 #include <pch.h>
 #include <utils/ThreadPool.h>
 #include <utils/Log.h>
+
+#include <cassert>
 #include <memory>
 
 namespace t850 {
 
 static std::unique_ptr<ThreadPool> s_globalPool;
+thread_local ThreadPool* ThreadPool::s_currentWorkerPool = nullptr;
 ThreadPool* g_threadPool = nullptr;
+
+bool ThreadPool::IsWorkerThread() const {
+  return s_currentWorkerPool == this;
+}
+
+bool ThreadPool::CanBlockFromCurrentThread(const char* operation) const {
+  if (!IsWorkerThread()) return true;
+
+  T8_LOG_ERROR("[ThreadPool] %s called from a worker thread; this violates the blocking-call contract", operation);
+  assert(false && "ThreadPool blocking call from worker thread");
+  return false;
+}
 
 void InitGlobalThreadPool() {
   if (!g_threadPool) {
